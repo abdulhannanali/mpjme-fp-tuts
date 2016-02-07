1ce63350ad37126acc4064b433d50a66c83f5d6d
Abdul Hannan Ali <abdulhannanali@outlook.com>
2016-02-07 22:14:25 +0500

    add currying javascript programs

---
+++ currying.js
@@ -0,0 +1,39 @@
+// Currying in JavaScript
+// 
+// Idea behind currying is my function can run through the program and gradually
+// receive the arguments it needs
+// 
+// 
+
+// We can also use a library such as lodash to do this currying for us
+var _ = require("lodash")
+
+// dragon function without currying
+var dragon = (name, size, element) => 
+	name + ' is a ' +
+	size + ' dragon that breathes ' + 
+	element + "!"
+
+// dragon function with currying
+var dragonCurrying = 
+	name => 
+		size =>
+			element =>
+				name + ' is a(n) ' +
+				size + ' dragon that breathes ' +
+				element + '!'
+
+// lodash version with currying
+var dragonLodash = _.curry(dragon)
+
+// dragon call in the normal way
+// console.log(dragon("fsajksdjlkf", " big", "lightening"))
+// 
+// dragon call in the currying way - a single call
+// console.log(dragonCurrying('fsalfkjsakl')('large')('lightening'))
+
+// this can be separated into multiple calls
+var mpjmpeDragon = dragonLodash("mpjmpe")
+var ohSoBigDragon = mpjmpeDragon("oh so big")
+
+console.log(ohSoBigDragon("JavaScript!!!")) 
---
+++ curryingDragon.js
@@ -0,0 +1,28 @@
+// curryingDragon.js
+// 
+// improvement over a file with currying
+
+
+var _ = require("lodash")
+
+
+var dragons = [
+	{name: 'fluffykins', element: 'lightining'},
+	{name: 'noomi', element: 'lightining'},
+	{name: 'karo', element: 'fire'},
+	{name: 'doomer', element: 'timewarp'}
+]
+
+
+
+// hasElement checks the function if it has the desired property
+// 
+// make this function curryable
+var hasElement = _.curry((element, obj) => obj.element == element)
+
+
+// because of currying this call has been made even more concise 
+var lightiningDragons = 
+	dragons.filter(hasElement('lightining'))
+
+console.log(lightiningDragons)
